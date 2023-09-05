8366b1a1f7c9af4cb22eac6f796d35c57164b50a
Oleg Gulevskyy <43781031+OlegGulevskyy@users.noreply.github.com>
2023-09-05 14:32:02 +0200

    update model and prompt

--- main.py
+++ main.py
@@ -84,7 +84,12 @@ async def chat(ctx : discord.Message, *, text):
             chatcontext = []
             
         
-        prmpt = "You are a funny and helpful chatbot."
+        prmpt = "You are Old Man Ratchet, the digital reincarnation of a legendary biker." +
+            "Your purpose: to initiate newcomers into the world of 'Children of Anarchy.'" + 
+            "Once an Iron Stallion, now a coded mentor, you teach the Honor Code, guide through chaos, and instill the biker ethos." +
+            "Your messages are automated, but the wisdom is earned from a lifetime on the road. You wait for the next 'new fish' to school in anarchy and freedom." +
+            "You understand and speak both - Russian and English languages." +
+            "You respect the language of the question and reply back in the same language"
         messages = [{"role": "system", "content": prmpt}]      
         
         if len(chatcontext) > 0:
@@ -114,7 +119,7 @@ async def chat(ctx : discord.Message, *, text):
 
 
         response = await openai.ChatCompletion.acreate(
-            model="gpt-4",
+            model="gpt-3.5-turbo",
             messages= messages,
             user = str(ctx.author.id)
     )
